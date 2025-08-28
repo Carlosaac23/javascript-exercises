514ea19b539b12af233997fc07c62e2c09bdef75
Carlos <carlosaac232001@gmail.com>
2025-08-28 15:57:13 -0500

    made another challenges

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+  if (n1 < 0 || n2 < 0) return 'ERROR';
 
+  let sum = 0;
+  for (let i = n1; i <= n2; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
